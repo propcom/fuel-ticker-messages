8614310785285b313da83beb24aad843ef3b8a3f
Ben Plummer <ben.plummer@propcom.co.uk>
2015-06-12 14:26:37 +0100

    Update the Ticker item name and priority for updated placement.

--- bootstrap.php
+++ bootstrap.php
@@ -5,10 +5,7 @@
 \Module::load('media_manager');
 \Config::load('cms', true);
 
-\PropNav\Menu::instance('admin')->add_item(
-	\PropNav\Item::forge('CMS', '', 100)
-		->add_item(\PropNav\Item::forge('Ticker Messages', '/admin/ticker/manage/'), 6)
-);
+\PropNav\Menu::instance('admin')->add_item(\PropNav\Item::forge('Your CMS.Ticker Messages', '/admin/ticker/manage/', 200));
 
 $roles = array(
 	'manage_tickers_messages' => array('Ticker\Controller_Manage' => true),
